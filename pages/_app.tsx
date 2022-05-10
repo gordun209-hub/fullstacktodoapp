6d60767dc92db05dbf51378127374d41092157cc
gordun209 <gordun209@hotmail.com>
2022-05-10 18:56:05 +0300

    add layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import '../styles/globals.css'
 
-import { ChakraProvider } from '@chakra-ui/provider'
-import { extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import type { FC } from 'react'
 import React from 'react'
@@ -11,15 +9,6 @@ import { ReactQueryDevtools } from 'react-query/devtools'
 import Layout from '@/components/Layout'
 
 // 2. Extend the theme to include custom colors, fonts, etc
-const colors = {
-	brand: {
-		900: '#1a365d',
-		800: '#153e75',
-		700: '#2a69ac'
-	}
-}
-
-const theme = extendTheme({ colors })
 
 const MyApp: FC<AppProps> = ({ Component, pageProps: { ...pageProps } }) => {
 	// eslint-disable-next-line react/hook-use-state
@@ -27,11 +16,9 @@ const MyApp: FC<AppProps> = ({ Component, pageProps: { ...pageProps } }) => {
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<ChakraProvider theme={theme}>
-				<Layout />
-				<Component {...pageProps} />
-				<ReactQueryDevtools />
-			</ChakraProvider>
+			<Layout />
+			<Component {...pageProps} />
+			<ReactQueryDevtools />
 		</QueryClientProvider>
 	)
 }
