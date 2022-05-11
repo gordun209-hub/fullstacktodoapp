ae6fbf12f9135c6143551c619806e95e6604125e
Mithat Ercan <mithat.ercan@hotmail.com>
2022-05-11 11:59:24 +0300

    Fix the layout issue

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,56 +1,33 @@
-
 import { Box } from '@mui/material'
 import { useRouter } from 'next/router'
 import { useQuery } from 'react-query'
 
 import MainContent from '@/components/MainContent'
 import MainHero from '@/components/MainHero'
-import Signup from '@/components/Signup'
-import { getUserQuery } from '@/services/api'
 
-import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
-import Typography from '@mui/material/Typography'
 
-import { AvatarComponent, SignupForm } from '@/components/index'
-
+import { SignupForm } from '@/components/index'
 
 const theme = createTheme()
 
 const SignUp: () => JSX.Element = () => {
 	return (
-
-        	<div>
+		<div>
 			<Box
 				sx={{
 					display: 'flex',
 					height: '100vh'
 				}}
 			>
-		<ThemeProvider theme={theme}>
-			<Container component='main' maxWidth='xs'>
-				<Box
-					sx={{
-						marginTop: 8,
-						display: 'flex',
-						flexDirection: 'column',
-						alignItems: 'center'
-					}}
-				>
-          <MainHero path='/auth.jpg' />
-					<AvatarComponent />
-					<Typography component='h1' variant='h5'>
-						Sign up
-					</Typography>
-					<SignupForm />
-				</Box>
-			</Container>
-		</ThemeProvider>
-        	</MainContent>
+				<MainHero path='/auth.jpg' />
+				<MainContent>
+					<ThemeProvider theme={theme}>
+						<SignupForm />
+					</ThemeProvider>
+				</MainContent>
 			</Box>
 		</div>
-
 	)
 }
 
